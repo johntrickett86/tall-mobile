6d1f79493c4adb5db9ce4d2362b7ea8ef4593b5e
John Trickett <johntrickett86@gmail.com>
2024-07-11 16:50:32 +0100

    Update login.blade.php

--- stubs/auth/resources/views/livewire/auth/login.blade.php
+++ stubs/auth/resources/views/livewire/auth/login.blade.php
@@ -3,7 +3,7 @@
 <div>
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
         <a href="{{ route('home') }}">
-            <x-logo class="w-auto h-16 mx-auto text-primary-600 dark:text-primary-400" />
+            <x-logo class="w-auto h-16 mx-auto text-primary-600 dark:text-primary-500" />
         </a>
 
         <h2 class="mt-6 text-3xl font-extrabold text-center text-light-900 dark:text-white leading-9">
